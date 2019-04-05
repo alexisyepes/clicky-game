6423e91d8d12c626fe21b514bf24d9d8d156d5af
Alex Yepes <alexisyepes@yahoo.com>
2019-04-05 16:17:38 -0400

    Fixing reset link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "../styles/Navbar.css";
+import { Link } from "react-router-dom";
 
 function Navbar(props) {
     return (
         <div className="topBar">
             <div className="row">
                 <div className="col-md-2">
-                    <a id="homeLink" href="/clicky-game"><h2>Reset</h2></a>
+                    <Link to="/" className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}>
+                        Reset
+                    </Link>
                 </div>
                 <div className="col-md-5">
                     <h2>Click an Image to begin!</h2>
