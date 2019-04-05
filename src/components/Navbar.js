4896ef1ce0038f3ce47e8a6f0b9352259d7964b3
Alex Yepes <alexisyepes@yahoo.com>
2019-04-05 19:44:28 -0400

    small changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,11 +5,13 @@ function Navbar(props) {
     return (
         <div className="topBar">
             <div className="row">
-                
-                <div className="col-md-4 navBarLeftText">
+                <div className="col-md-2">
+                    <a id="homeLink" href="/"><h2>Reset</h2></a>
+                </div>
+                <div className="col-md-5">
                     <h2>Click an Image to begin!</h2>
                 </div>
-                <div className="col-md-3">
+                <div className="col-md-2">
                     <h2>Score: {props.score}</h2>
                 </div>
                 <div className="col-md-3">
