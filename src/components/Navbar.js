aa1b523d91c1eb80a3a57145ecb1b8059c9a675e
Alex Yepes <alexisyepes@yahoo.com>
2019-04-05 22:02:26 -0400

    f f b

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ function Navbar(props) {
         <div className="topBar">
             <div className="row">
                 <div className="col-md-2">
-                    <a id="homeLink" href="/"><h2>Reset</h2></a>
+                    <a id="homeLink" href="/">Reset</a>
                 </div>
                 <div className="col-md-5">
                     <h2>Click an Image to begin!</h2>
