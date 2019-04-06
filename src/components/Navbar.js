b9e9f26e9c2a5bf923c157009a7dd3272771312a
Alex Yepes <alexisyepes@yahoo.com>
2019-04-05 20:46:47 -0400

    one small change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,13 +8,13 @@ function Navbar(props) {
                 <div className="col-md-2">
                     <a id="homeLink" href="/"><h2>Reset</h2></a>
                 </div>
-                <div className="col-md-5 navText">
+                <div className="col-md-5">
                     <h2>Click an Image to begin!</h2>
                 </div>
-                <div className="col-md-2 navText">
+                <div className="col-md-2">
                     <h2>Score: {props.score}</h2>
                 </div>
-                <div className="col-md-3 navText">
+                <div className="col-md-3">
                     <h2>Top Score: {props.highScore}</h2>
                 </div>
             </div>
