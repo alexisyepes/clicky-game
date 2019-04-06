058b19ebff4fdb0da9b75fb21d4ce4d41f0b6384
Alex Yepes <alexisyepes@yahoo.com>
2019-04-05 21:15:24 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/clicky-game/precache-manifest.069ade2b0cc1dba82101911e33739398.js"
+  "/clicky-game/precache-manifest.26ffb653240ce419152061496845c1ba.js"
 );
 
 workbox.clientsClaim();
