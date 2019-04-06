cecf03626b3dc85ee41e819257924b81715482d5
Alex Yepes <alexisyepes@yahoo.com>
2019-04-05 20:51:37 -0400

    Updates

--- precache-manifest.069ade2b0cc1dba82101911e33739398.js
+++ precache-manifest.069ade2b0cc1dba82101911e33739398.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
   },
   {
-    "revision": "358862557ace3b30db6b",
-    "url": "/clicky-game/static/js/main.6e7b23b8.chunk.js"
+    "revision": "7499a3217809479ffd17",
+    "url": "/clicky-game/static/js/main.bbf6c44e.chunk.js"
   },
   {
     "revision": "dd0ed99a380f8d225736",
     "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
   },
   {
-    "revision": "358862557ace3b30db6b",
+    "revision": "7499a3217809479ffd17",
     "url": "/clicky-game/static/css/main.f2b7ced6.chunk.css"
   },
   {
-    "revision": "e506469a39063e4325a82a2d4a074d19",
+    "revision": "d82a8478e3809b9b6bf6b8390a548942",
     "url": "/clicky-game/index.html"
   }
 ];
